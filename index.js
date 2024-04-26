4c89c72002d12042aab58bab1918281c1cfb79d7
ash <getseth00@gmail.com>
2024-04-26 22:49:25 +0530

    console buffer data

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-import { writeFile } from 'node:fs';
-import { Buffer } from 'node:buffer';
-import { readFile } from 'node:fs';
+import { writeFile } from "node:fs";
+import { Buffer } from "node:buffer";
+import { readFile } from "node:fs";
 
-readFile('./message.txt','utf8', (err, data) => {
+readFile("./message.txt", "utf8", (err, data) => {
   if (err) throw err;
 
   // Convert data to buffer
   const bufferdata = new Uint8Array(Buffer.from(data));
+  console.log(bufferdata);
   // Write buffer data to file
-writeFile('data.txt', data, (err) => {
+  writeFile("data.txt", data, (err) => {
     if (err) throw err;
-    console.log('The file has been saved!');
+    console.log("The file has been saved!");
   });
 });
-
-
